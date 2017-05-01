607e2bdb8557fa481ac5818f133d3c718fb19a73
Luke <Luke@admins-iMac-2.local>
2017-05-01 14:57:21 -0700

    Implemented show functionality for portfolio module

--- app/controllers/portfolio_contents_controller.rb
+++ app/controllers/portfolio_contents_controller.rb
@@ -3,4 +3,44 @@ class PortfolioContentsController < ApplicationController
 		@portfolio_items = PortfolioContent.all
 	end
 
+	def show 
+		@portfolio_item = PortfolioContent.find(params[:id])
+	end
+
+	def new
+		@portfolio_item = PortfolioContent.new
+	end
+
+	def create
+		@portfolio_item = PortfolioContent.find(params[:id])
+
+		respond_to do |format|
+		if @portfolio_item.save
+			formatl.html { redirect_to portfolioContents_path, notice: "Your item is now live" }
+		else
+			format.html { render :new}
+			end
+		end
+	end
+
+	def edit
+		@portfolio_item = PortfolioContent.find(params[:id])
+	end
+
+	 def update
+	  	@portfolio_item = PortfolioContent.find(params[:id])
+    	respond_to do |format|
+      	if @portfolio_item.update(:portfolioContent_params)
+        format.html { redirect_to portfolioContents_path, notice: 'Blog was successfully updated.' }
+      	else
+        format.html { render :edit }
+      end
+    end
+  end
+
+	private 
+	def portfolioContent_params
+		params.require(:portfolioContent).permit(:title, :subtitle, :body)
+		
+	end
 end
