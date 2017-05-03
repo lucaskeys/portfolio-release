5ebe9f00b4018a87b8c038ee6e3f3def01c64603
Luke <Luke@admins-iMac-2.local>
2017-05-03 11:47:44 -0700

    Integrated tech model

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,3 +33,9 @@ end
 
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+	PortfolioContent.last.technologies.create!(name: "Technology #{technology}")
+end
+
+puts "3 technologies created"
