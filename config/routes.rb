47704c1618f292f07ef9e923ce134e7a45ee9f95
Luke <Luke@admins-iMac-2.local>
2017-05-03 14:46:30 -0700

    Customize reg paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolio_contents, except: [:show]
   get 'angular-items', to: 'portfolio_contents#angular'
   get 'porfolio/:id', to: 'portfolio_contents#show', as: 'portfolio_show'
