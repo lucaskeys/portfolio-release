e9ed74c42031d5dbae600c8656646096dc9104c9
Luke <Luke@admins-iMac-2.local>
2017-05-03 15:53:55 -0700

    complete auth with custom attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name 
+  	# self.name is sayig the specific name for the current user we are talking about
+  	# whenever we are instantiating a specific user
+  	self.name.split.first
+  end
+
+  def last_name
+  	self.name.split.last
+  end
 end
