081111e1c0395e8d0b086c3ae14b625ac6f3ef3b
Luke <Luke@admins-iMac-2.local>
2017-05-04 14:31:46 -0700

    Implemented collection partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
 	   		(link_to "Login", new_user_session_path) 
 	   end 
 	end
+
+	def sample_helper 
+		content_tag(:div, "My content", class: "my-class")
+	end
+
+	def source_helper(layout_name)
+		if session[:source]
+			greeting = "Thanks for visiting me from #{sessions[:source]} and you are on the #{layout_name} layout"
+			content_tag(:p, greeting, class: "source-greeting")
+		end
+	end
 end
