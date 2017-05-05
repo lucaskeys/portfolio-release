98e84f1ff58c162f38ddfc07606b72a1bfe6b7f4
Luke <Luke@admins-iMac-2.local>
2017-05-05 15:55:35 -0700

    Add user authorization

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
   # can decide which layout to use
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
