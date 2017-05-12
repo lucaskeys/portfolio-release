b615e23980ba3d94afd8f7501179b93bd29ae67e
Luke <Luke@admins-iMac-2.local>
2017-05-11 17:00:52 -0700

    Implemented topic widget for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout "blog"
   # can decide which layout to use
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
@@ -103,4 +104,8 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
     end
+
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
+  end  
 end
